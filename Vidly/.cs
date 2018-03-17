a63762a9304e95cc711fedfecdfa394ecd72599b
YANG XU <xuyangxinyi@hotmail.com>
2018-03-17 18:00:48 +1100

    Commit Authentication

---
+++ Vidly/.cs
@@ -0,0 +1,31 @@
+namespace Vidly
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Movie : DbContext
+    {
+        // Your context has been configured to use a 'Movie' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Vidly.Movie' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Movie' 
+        // connection string in the application configuration file.
+        public Movie()
+            : base("name=Movie")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
